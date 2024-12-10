f49f838436616b08449ba2e4d44d1a52a46245db
Gabriel Pinheiro <gabriel.pinheiro1995@gmail.com>
2024-12-10 12:03:38 -0300

    Atualização 10/12

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
 import fastify from 'fastify';
+import { knex } from './database';
 
 const app = fastify();
 
-app.get('/hello', () => {
-   return 'Hello Node';
-},);
+app.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*');
 
-app.listen({
-   port: 3333,
-},).then(() => {
-   console.log('HTTP Server Running!',);
-},);
+  return tables;
+});
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server Running!');
+  });
