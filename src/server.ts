47b35f23913d69a95b59c74027d8644aa7e94952
Gabriel Rosa Pinheiro <gabriel.pinheiro1995@gmail.com>
2025-02-16 14:49:04 -0300

    host render

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import { app } from './app';
 import { env } from './env';
 
 app
-  .listen({ port: env.PORT })
+  .listen({
+    port: env.PORT,
+    host: env.HOST,
+  })
   .then(() => {
     console.log(`HTTP Server Running in port ${env.PORT}`);
   });
